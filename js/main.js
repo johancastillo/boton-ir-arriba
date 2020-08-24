69c9ca28e6e58aa8edc38080da495327b0c9a485
Johan Castillo <jcjohyan2707@gmail.com>
2020-08-24 14:03:54 -0400

    Efecto de aparecer y desaparecer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 
-  $('.ir-arriba').click(function(){
-    $('body, html').animate({
-      scrollTop: '0px'
-    }, 300);
-  });
+  //Funcion de ir arriba
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+  //Efecto de aparecer y desaparecer
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
 
 });
